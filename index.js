b8aba9a3d12ed93d42ea2b962c521f2f8e099b07
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-04-26 06:47:27 -0700

    added responsive css

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const inquirer = require("inquirer");
 
+const fs = require("fs");
+const util = require("util");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
